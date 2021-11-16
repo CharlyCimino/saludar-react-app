3041b9970fa25439d824a25f75df38196b6a684f
Charly Cimino <carlosecimino@gmail.com>
2021-11-16 10:09:50 -0300

    Eliminada indicación de tarea

--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import SaludadorApp from './components/SaludadorApp';
 
 const divRoot = document.querySelector("#root");
 
-ReactDOM.render(<SaludadorApp anioDeNacimiento={1993} />, divRoot);
-/*
-    TAREA: Poner un form que pida nombre y año de nacimiento al cliente y al hacer
-    'submit' se muestre el saludador en tiempo real (sin recargar la página)
-*/
+ReactDOM.render(<SaludadorApp />, divRoot);
